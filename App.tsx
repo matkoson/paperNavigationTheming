a41bdb430013debcc08fb100ecfe4d3029248ace
Mateusz Koson <mateusz.koson@callstack.com>
2020-04-24 09:10:32 +0200

    feat: set up dark mode

--- App.tsx
+++ App.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native";
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -9,30 +18,37 @@ import Details from "./src/Screens/CardDetailsScreen";
 import CardScreen from "./src/Screens/CardListScreen";
 import { Routes } from "./routes";
 
+import merge from "deepmerge";
+
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={Routes.list}
-          headerMode="screen"
-          screenOptions={{
-            header: ({ scene, previous, navigation, ...rest }) => (
-              <Header
-                scene={scene}
-                previous={previous}
-                navigation={navigation}
-                {...rest}
-              />
-            ),
-          }}
-        >
-          <Stack.Screen name={Routes.list} component={CardScreen} />
-          <Stack.Screen name={Routes.details} component={Details} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={CombinedDarkTheme}>
+        <NavigationContainer theme={CombinedDarkTheme}>
+          <Stack.Navigator
+            initialRouteName={Routes.list}
+            headerMode="screen"
+            screenOptions={{
+              header: ({ scene, previous, navigation, ...rest }) => (
+                <Header
+                  scene={scene}
+                  previous={previous}
+                  navigation={navigation}
+                  {...rest}
+                />
+              ),
+            }}
+          >
+            <Stack.Screen name={Routes.list} component={CardScreen} />
+            <Stack.Screen name={Routes.details} component={Details} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
